4b103b7da99e5d4752e3a724444165e204798764
keolaiseri <iserik@hawaii.edu>
2022-09-22 08:04:23 -1000

    edit

--- app/src/main/java/com/example/android/calculator/MainActivity.kt
+++ app/src/main/java/com/example/android/calculator/MainActivity.kt
@@ -2,10 +2,32 @@ package com.example.android.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
+
+    val operationList: MutableList<String> = arrayListOf()
+    val numberCache: MutableList<String> = arrayListOf()
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        updateDisplay("")
+    }
+
+
+
+    fun updateDisplay(mainString: String) {
+    }
+
+    fun clearCache(){
+        numberCache.clear()
+        operationList.clear()
+    }
+
+    fun clear(view: View){
+        clearCache()
+        updateDisplay("")
     }
 }
